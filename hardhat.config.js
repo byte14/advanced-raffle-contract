d8da3c12ec760353c4ea9a4049090b378cfcb09d
byte14 <avishekrajpanta@gmail.com>
2022-08-27 23:27:18 +0545

    Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,26 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmtions: 1,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmtions: 6,
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 0,
+    },
+  },
+  solidity: "0.8.7",
 };
