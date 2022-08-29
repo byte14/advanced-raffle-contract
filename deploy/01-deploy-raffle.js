b36614bb12adf1bd49353360003fc78ca23936cc
byte14 <avishekrajpanta@gmail.com>
2022-08-29 20:21:49 +0545

    Added consumer for VRFCoordinatorV2Mock

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,8 +1,5 @@
 const { network, ethers } = require("hardhat");
-const {
-  developmentChains,
-  networkConfig,
-} = require("../helper-hardhat-config");
+const { networkConfig } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
 const FUND_AMOUNT = ethers.utils.parseEther("1");
@@ -11,30 +8,29 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
-  let vrfCoordinatorV2Address, subscriptionId;
+  let vrfCoordinatorV2Mock, vrfCoordinatorV2Address, subscriptionId;
 
-  if (developmentChains.includes(network.name)) {
-    const vrfCoordinatorV2Mock = await ethers.getContract(
-      "VRFCoordinatorV2Mock"
-    );
+  if (chainId === 31337) {
+    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
     const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
     const transactionReceipt = await transactionResponse.wait(1);
     subscriptionId = await transactionReceipt.events[0].args.subId;
+    console.log(subscriptionId.toString());
     await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
     subscriptionId = networkConfig[chainId]["subscriptionId"];
   }
 
-  const entranceFee = networkConfig[chainId]["entranceFee"];
+  const entryFee = networkConfig[chainId]["entryFee"];
   const keyHash = networkConfig[chainId]["keyHash"];
   const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
   const interval = networkConfig[chainId]["interval"];
 
   const args = [
     vrfCoordinatorV2Address,
-    entranceFee,
+    entryFee,
     keyHash,
     subscriptionId,
     callbackGasLimit,
@@ -48,17 +44,14 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     waitConfirmations: network.config.blockConfirmations || 1,
   });
 
-  // if (developmentChains.includes(network.name)) {
-  //   await vrfCoordinatorV2Mock.addConsumer(
-  //     subscriptionId.toNumber(),
-  //     raffle.address
-  //   );
-  // }
+  if (chainId === 31337) {
+    await vrfCoordinatorV2Mock.addConsumer(
+      subscriptionId.toNumber(),
+      raffle.address
+    );
+  }
 
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
+  if (chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
     await verify(raffle.address, args);
   }
   log("______________________________________________");
