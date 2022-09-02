95db3c9745a717ede53b302d8f2cf66e88f95b74
byte14 <avishekrajpanta@gmail.com>
2022-09-02 21:25:53 +0545

    fund Chainlink VRF subscription

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,8 +1,9 @@
 const { network, ethers } = require("hardhat");
 const { networkConfig } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
+require("dotenv").config();
 
-const FUND_AMOUNT = ethers.utils.parseEther("1");
+const SEND_VALUE = ethers.utils.parseEther("2");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
@@ -10,21 +11,15 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const chainId = network.config.chainId;
   let vrfCoordinatorV2Mock;
   let vrfCoordinatorV2Address;
-  let linkToken;
-  let subscriptionId;
+  let linkTokenAddress;
 
   if (chainId === 31337) {
     vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
-    linkToken = (await ethers.getContract("LinkToken")).address;
-    const txResponse = await vrfCoordinatorV2Mock.createSubscription();
-    const txReceipt = await txResponse.wait(1);
-    subscriptionId = await txReceipt.events[0].args.subId;
-    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
+    linkTokenAddress = (await ethers.getContract("LinkToken")).address;
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
-    linkToken = networkConfig[chainId]["linkToken"];
-    subscriptionId = networkConfig[chainId]["subscriptionId"];
+    linkTokenAddress = networkConfig[chainId]["linkToken"];
   }
 
   const entryFee = networkConfig[chainId]["entryFee"];
@@ -34,30 +29,36 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
   const arguments = [
     vrfCoordinatorV2Address,
-    linkToken,
+    linkTokenAddress,
     entryFee,
     keyHash,
-    subscriptionId,
     callbackGasLimit,
     interval,
   ];
 
-  const raffle = await deploy("Raffle", {
+  await deploy("Raffle", {
     from: deployer,
     args: arguments,
     log: true,
     waitConfirmations: network.config.blockConfirmations,
   });
 
-  if (chainId === 31337) {
-    await vrfCoordinatorV2Mock.addConsumer(
-      subscriptionId.toNumber(),
-      raffle.address
-    );
-  }
+  const raffle = await ethers.getContract("Raffle", deployer);
 
-  if (chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
-    await verify(raffle.address, args);
+  if (chainId === 31337) {
+    await vrfCoordinatorV2Mock.fundSubscription(1, SEND_VALUE);
+  } else {
+    const linkToken = await ethers.getContractAt("LinkToken", linkTokenAddress);
+    const approveLinkTX = await linkToken.approve(raffle.address, SEND_VALUE);
+    await approveLinkTX.wait(1);
+    const depositLinkTx = await raffle.depositLink(SEND_VALUE);
+    await depositLinkTx.wait(1);
+    const fundSubscriptionTx = await raffle.fundSubscription(SEND_VALUE);
+    await fundSubscriptionTx.wait(1);
+
+    if (process.env.ETHERSCAN_API_KEY) {
+      await verify(raffle.address, arguments);
+    }
   }
   log("______________________________________________________");
 };
