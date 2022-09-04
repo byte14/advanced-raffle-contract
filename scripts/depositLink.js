a3dc82a0551e8ff1c39637b59d6757c550df1edb
byte14 <avishekrajpanta@gmail.com>
2022-09-04 13:33:47 +0545

    added script for depositing link

---
+++ scripts/depositLink.js
@@ -0,0 +1,29 @@
+const { ethers, network } = require("hardhat");
+const { networkConfig } = require("../helper-hardhat-config");
+
+const sendValue = ethers.utils.parseEther("10");
+const chainId = network.config.chainId;
+const linkTokenAddress = networkConfig[chainId]["linkToken"];
+let linkToken;
+
+async function depositLink() {
+  const raffle = await ethers.getContract("Raffle");
+  if (chainId !== 31337) {
+    linkToken = await ethers.getContractAt("LinkToken", linkTokenAddress);
+  } else {
+    linkToken = await ethers.getContract("LinkToken");
+  }
+  const approveLinkTx = await linkToken.approve(raffle.address, sendValue);
+  await approveLinkTx.wait(1);
+  console.log(`${sendValue} Link token has been approved`);
+  const depositLinkTx = await raffle.depositLink(sendValue);
+  await depositLinkTx.wait(1);
+  console.log(`${sendValue} Link token has been deposited`);
+}
+
+depositLink()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
